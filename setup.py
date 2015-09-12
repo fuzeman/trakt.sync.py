fe4128b7c6608e1457dfaa98ee3ea0aa6dacc484
Dean Gardiner <gardiner91@gmail.com>
2015-09-12 22:09:44 +1200

    Added [setup.py]

---
+++ setup.py
@@ -0,0 +1,30 @@
+from trakt_sync import __version__
+
+from setuptools import setup, find_packages
+
+setup(
+    name='trakt.sync.py',
+    version=__version__,
+    license='MIT',
+    url='https://github.com/fuzeman/trakt.sync.py',
+
+    author='Dean Gardiner',
+    author_email='me@dgardiner.net',
+
+    description='Sync extension for trakt.py',
+    packages=find_packages(exclude=[
+        'examples'
+    ]),
+    platforms='any',
+
+    install_requires=[
+        'trakt.py'
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+)
